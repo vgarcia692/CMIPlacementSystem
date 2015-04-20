236e8a446c8307056785c11494f80cb415d23fac
Victor <victor.lemae.garcia@gmail.com>
2015-04-21 08:55:39 +1200

    initial build

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/placement';
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres:admin@172.16.0.56:5432/placement';
 
 module.exports = connectionString;
